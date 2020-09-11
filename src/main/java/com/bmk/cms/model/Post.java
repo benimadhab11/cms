d03d62b9650c83dc7ef5fab3e518899d6c97732a
Beni Madhab Kayal <benimadhabkayal@Benis-MacBook-Pro.local>
2020-09-12 00:19:01 +0530

    r

--- src/main/java/com/bmk/cms/model/Post.java
+++ src/main/java/com/bmk/cms/model/Post.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.Type;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -29,6 +30,7 @@ public class Post implements Serializable {
     private String title;
 
     @Lob
+    @Type(type = "org.hibernate.type.TextType")
     private String content;
     private String author;
     private String status;
